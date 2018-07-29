0db7385989e1f79ddfcda69f071da80d60d81de9
dwightellis214 <dwight.g.ellis@gmail.com>
2018-07-28 23:45:45 -0400

    Added comments, login features, delete and publish cacapability

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from django.conf.urls import include, url
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'),
     path('', include('blog.urls')),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
